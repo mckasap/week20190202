3b0c7f2ec43bdd680f7b08c82f859e5540717c82
mckasap <mckasap@gmail.com>
2019-02-15 16:30:37 +0300

    Intent implicit

--- app/src/main/java/org/kasapbasi/week2019_0201/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week2019_0201/MainActivity.java
@@ -1,13 +1,24 @@
 package org.kasapbasi.week2019_0201;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView im;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        im=(ImageView) findViewById(R.id.imageView);
+        im.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+
     }
 }
